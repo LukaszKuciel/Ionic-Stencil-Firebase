7ee9dc5b0b1d94dea84670f83457356ac17c1735
Lukasz Kuciel <lukaszkuciel95@gmail.com>
2019-04-28 23:17:17 +0200

    Admin page added

---
+++ src/components/page-admin/page-admin.tsx
@@ -0,0 +1,19 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+    tag: 'page-admin',
+    styleUrl: 'page-admin.scss'
+})
+export class PageAdmin {
+
+    
+
+    render() {
+        return (
+            <div>
+                <p>Hello PageAdmin!</p>
+            </div>
+        );
+    }
+}
