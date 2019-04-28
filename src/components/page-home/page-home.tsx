8a77af0dc51628cc61841337e453d0be822b421a
Lukasz Kuciel <lukaszkuciel95@gmail.com>
2019-04-28 23:19:23 +0200

    User interface update from firebase to custom

--- src/components/page-home/page-home.tsx
+++ src/components/page-home/page-home.tsx
@@ -1,20 +1,21 @@
-import { Component, Prop } from '@stencil/core';
-import { Navbar } from '../functional';
+import { Component, Prop } from "@stencil/core";
+import { Navbar } from "../functional";
+import { User } from "../../interfaces/user";
 
 @Component({
-    tag: 'page-home',
-    styleUrl: 'page-home.scss'
+  tag: "page-home",
+  styleUrl: "page-home.scss"
 })
 export class PageHome {
-    @Prop() user: firebase.User
+  @Prop() user: User;
 
-    render() {
-        return [
-            <Navbar title='Home' />,
-            <ion-content>
-                <h1>{ this.user ? this.user.displayName : <ion-skeleton-text /> }</h1>
-                <p>{ this.user ? this.user.email : <ion-skeleton-text /> }</p>
-            </ion-content>
-        ];
-    }
+  render() {
+    return [
+      <Navbar title="Home" />,
+      <ion-content>
+        <h1>{this.user ? this.user.displayName : <ion-skeleton-text />}</h1>
+        <p>{this.user ? this.user.email : <ion-skeleton-text />}</p>
+      </ion-content>
+    ];
+  }
 }
